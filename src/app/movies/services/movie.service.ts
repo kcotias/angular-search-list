9762c62796153b7d92b8cfd91d59fbc7703179b1
kcotias <kaianexperia@gmail.com>
2019-06-07 06:41:33 -0300

    polish and fixes

--- src/app/movies/services/movie.service.ts
+++ src/app/movies/services/movie.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 
+
+interface Data {
+  results?: any;
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -17,7 +21,7 @@ export class MovieService {
   searchMovies(search: string) {
     let movies = [];
     this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=a80a7213ccea6ac0a1808fefae0d65ad&language=en-US&query=${search}&page=1&include_adult=false`)
-      .subscribe((data) => data.results.map((item) => {
+      .subscribe((data: Data) => data.results.map((item) => {
         let newLink = `http://image.tmdb.org/t/p/w185//${item.poster_path}`
         item.poster_path = newLink;
         movies.push(item);
